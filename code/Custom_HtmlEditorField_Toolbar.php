9f30aad58629b57588e922a7611474e0499a272c
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:26:53 +1300

    Converted to PSR-2

--- code/Custom_HtmlEditorField_Toolbar.php
+++ code/Custom_HtmlEditorField_Toolbar.php
@@ -7,36 +7,37 @@
  * @author Gabriele Brosulo <gabriele.brosulo@zirak.it>
  * @creation-date 10-Mar-2014
  */
-class Custom_HtmlEditorField_Toolbar extends Extension{
+class Custom_HtmlEditorField_Toolbar extends Extension
+{
 
-	/**
-	 * Aggiorna l'elenco delle possibili opzioni di link
-	 * @param Form $form
-	 * @return type
-	 */
-	public function updateLinkForm($form) {
-		
-		Requirements::javascript(LINKABLE_DATAOBJECTS_DIR . "/javascript/linkable-dataobjects.js");
-		
-		$fields = $form->Fields();
-		/* @var $linkTipe CompositeField */
-		$compositeField = $fields[1];
-		/* @var $linkTipe OptionsetField */
-		$linkTipe = $fields[1]->fieldByName("LinkType");
-		
-		$options = $linkTipe->getSource();
-		
-		$linkables = ClassInfo::implementorsOf('Linkable');
-		foreach ($linkables as $class) {
-			$identifier = str_replace('\\', '-', strtolower($class));			
-			$options[$identifier] = $class::LinkLabel();
-			$linkTipe->setSource($options);
+    /**
+     * Aggiorna l'elenco delle possibili opzioni di link
+     * @param Form $form
+     * @return type
+     */
+    public function updateLinkForm($form)
+    {
+        Requirements::javascript(LINKABLE_DATAOBJECTS_DIR . "/javascript/linkable-dataobjects.js");
+        
+        $fields = $form->Fields();
+        /* @var $linkTipe CompositeField */
+        $compositeField = $fields[1];
+        /* @var $linkTipe OptionsetField */
+        $linkTipe = $fields[1]->fieldByName("LinkType");
+        
+        $options = $linkTipe->getSource();
+        
+        $linkables = ClassInfo::implementorsOf('Linkable');
+        foreach ($linkables as $class) {
+            $identifier = str_replace('\\', '-', strtolower($class));
+            $options[$identifier] = $class::LinkLabel();
+            $linkTipe->setSource($options);
 
-			$dropdown = new DropdownField($identifier, _t('HtmlEditorField.NEWS', $class::LinkLabel()), $class::get()->map('ID', 'Title'));
-			$dropdown->addExtraClass('linkabledo');
-                        $compositeField->insertBefore($dropdown, 'Description');
-		}
-		
-		return $form;
-	}
+            $dropdown = new DropdownField($identifier, _t('HtmlEditorField.NEWS', $class::LinkLabel()), $class::get()->map('ID', 'Title'));
+            $dropdown->addExtraClass('linkabledo');
+            $compositeField->insertBefore($dropdown, 'Description');
+        }
+        
+        return $form;
+    }
 }
